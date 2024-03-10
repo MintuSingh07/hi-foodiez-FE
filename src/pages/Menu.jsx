5bef4862e0f46f5295e93df9e317dfce896d51e3
Mintu Singh <m49838149@gmail.com>
2024-03-10 21:24:50 +0530

    Quantity selector | UI upgrade v1.0.0 [MENU]

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
+import "../styles/Menu.css"
 
 const Menu = () => {
     const [items, setItems] = useState([]);
@@ -18,7 +19,14 @@ const Menu = () => {
                         tb_no: tb_no
                     }
                 });
-                setItems(response.data.fullMenu);
+                const menus = response.data.fullMenu.map((val, index) => {
+                    return {
+                        ...val,
+                        quantity: 0
+                    }
+                })
+                console.log(menus);
+                setItems(menus);
                 settb_No(tb_no)
             } catch (error) {
                 console.error('Error:', error);
@@ -29,7 +37,18 @@ const Menu = () => {
     }, []);
 
     const addItemToCart = (item) => {
-        !selectedItems.includes(item) ? setSelectedItems(prevItems => [...prevItems, item]) : null;
+        console.log(item, selectedItems);
+        let alreadyAdded = false;
+        for (let i = 0; i < selectedItems.length; i++) {
+            if (selectedItems[i].name == item.name) {
+                alreadyAdded = true;
+                break;
+            }
+        }
+        console.log(alreadyAdded);
+        if (alreadyAdded == false) {
+            setSelectedItems(prevItems => [...prevItems, item]);
+        }
     };
 
     const showConfirm = () => {
@@ -39,8 +58,12 @@ const Menu = () => {
     const placeOrder = async () => {
         try {
             if (selectedItems.length > 0) {
-                const itemNames = selectedItems.map(item => item.name);
-                console.log(itemNames);
+                const itemNames = selectedItems.map(item => {
+                    return {
+                        name: item.name,
+                        quantity: item.quantity
+                    }
+                });
                 const response = await fetch('http://localhost:8000/order', {
                     method: "POST",
                     headers: {
@@ -54,7 +77,7 @@ const Menu = () => {
                 console.log('Order placed successfully:', await response.json());
                 setOrderStatus('Order placed sucessfully');
                 setInterval(() => {
-                    window.location = "/order";
+                    // window.location = "/order";
                 }, 2000);
             }
             else {
@@ -64,23 +87,51 @@ const Menu = () => {
             console.error("Error is:", error);
         }
     }
-    return (
-        <div>
-            <h1>Table No: {tb_No}</h1>
-            {
-                items.map((item, index) => (
-                    <ul key={index}>
-                        <li>{item.name}</li>
-                        <li>{item.isVeg ? "Veg" : "Non-Veg"}</li>
-                        <li>{item.price}</li>
-                        <button onClick={() => addItemToCart(item)}>+</button>
-                    </ul>
-                ))
+
+    const addItems = (isPlus, index) => {
+        const snapshot = selectedItems.map((val, i) => {
+            if (i === index) {
+                return {
+                    ...val,
+                    quantity: isPlus ? val.quantity + 1 : val.quantity > 0 ? val.quantity - 1 : 0
+                }
             }
+            return val
+        })
+        setSelectedItems(snapshot)
+    }
+    return (
+        <div id='menu_container'>
+            <nav>
+                <img src="/logo.svg" alt="" />
+            </nav>
+            <section>
+                <p id='table_no'>Order for Table No: <span>{tb_No}</span></p>
+                {
+                    items.map((item, index) => (
+                        <div className='single_item' key={index}>
+                            <div id="details">
+                                <div className='image-holder' style={{ backgroundImage: `url(${item.image})` }}></div>
+                                <div id="item_detail">
+                                    <h2>{item.name}</h2>
+                                    <p>{item.ingredients + ""}</p>
+                                    <h5>{"$ " + item.price}</h5>
+                                </div>
+                            <button onClick={() => addItemToCart(item)}>Add to cart</button>
+                            </div>
+                        </div>
+                    ))
+                }
+            </section>
             <h2>Items in Cart:</h2>
             <ul>
-                {selectedItems.map((item, index) => (
-                    <li key={index}>{item.name}</li>
+                {selectedItems.length > 0 && selectedItems.map((item, index) => (
+                    <li key={index}>
+                        {item.name}
+                        <button onClick={() => addItems(true, index)}>+</button>
+                        <span>{item.quantity}</span>
+                        <button onClick={() => addItems(false, index)}>-</button>
+                    </li>
                 ))}
             </ul>
             <button onClick={showConfirm}>Place Order</button>
@@ -93,3 +144,4 @@ const Menu = () => {
 }
 
 export default Menu
+
--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 const Order = () => {
+  const [time, setTime] = useState(300);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(prevTime => {
+        if (prevTime === 0) {
+          clearInterval(interval);
+          return 0;
+        } else {
+          return prevTime - 1;
+        }
+      });
+    }, 1000);
+
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
   return (
-    <div>Order</div>
-  )
+    <>
+      <p>Your Order will be at your table shortly...</p>
+      <p>Time remaining: {minutes} minute{minutes !== 1 ? 's' : ''} {seconds} second{seconds !== 1 ? 's' : ''}</p>
+    </>
+  );
 }
 
-export default Order
+export default Order;
